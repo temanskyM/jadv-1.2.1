03078b6e67a9c8861da2290dbdf0deda2ff185fc
Artiom <temansky@yandex.ru>
2021-03-26 21:20:07 +0300

    Initial commit

---
+++ src/main/java/Dealership.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private List<String> carList = new ArrayList<String>();
+
+    public Dealership() {
+
+    }
+
+    public synchronized void produceOneCar(String name) {
+        carList.add(name);
+        notify();
+    }
+
+    public synchronized void getOneCar() {
+        if (carList.size() == 0) {
+            System.out.println("Покупатель " + Thread.currentThread().getName() + ": у диллера нет машин, ожидаю поставки.");
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        carList.remove(0);
+        System.out.println("Покупатель " + Thread.currentThread().getName() + ": купил новую машину.");
+    }
+}
